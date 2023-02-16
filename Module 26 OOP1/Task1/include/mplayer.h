54d7681b8dc646b4b9cb849de082c22e7f6651e5
Artem Ulyanov <aiden_pride@mail.ru>
2023-02-17 00:40:13 +0300

    25 ht1 1 changes

--- Module 26 OOP1/Task1/include/mplayer.h
+++ Module 26 OOP1/Task1/include/mplayer.h
@@ -4,15 +4,32 @@
 
 #include "track.h"
 #include <vector>
+#include <string>
 
 #ifndef OOP_HT1_MPLAYER_H
 #define OOP_HT1_MPLAYER_H
 
+enum class pState {
+    PLAYBACK,
+    PAUSED,
+    STOPPED
+};
+
 class MPlayer {
     std::vector<Track *> trackList;
+    int curtrack = -1;
+    pState pstate = pState::STOPPED;
+
 public:
-    int getListSize();
+    unsigned int getListSize();
+    pState getPlaybackState();
     void showTrackList();
+    void stop();
+    void pause();
+    void play();
+    void play(int trackidx);
+    void next();
+    void addtrack(std::string tname, std::string dateCr, unsigned int length );
 };
 
 #endif //OOP_HT1_MPLAYER_H
--- Module 26 OOP1/Task1/src/mplayer.cpp
+++ Module 26 OOP1/Task1/src/mplayer.cpp
@@ -4,9 +4,14 @@
 
 #include <iostream>
 #include "mplayer.h"
+#include "track.h"
 
-int MPlayer::getListSize() {
+unsigned int MPlayer::getListSize() {
     return trackList.size();
+}
+
+pState MPlayer::getPlaybackState() {
+    return pstate;
 }
 
 void MPlayer::showTrackList() {
@@ -19,4 +24,43 @@ void MPlayer::showTrackList() {
         std::cout << i << "   " << trackList[i]->getName() << "   " << trackList[i]->getDateCreated().tm_mday << "." <<
         trackList[i]->getDateCreated().tm_mon << "." << trackList[i]->getDateCreated().tm_year << "   " << trackList[i]->getLength() << "sec.\n";
     }
+}
+
+void MPlayer::stop() {
+    if (pstate == pState::STOPPED) return;
+    pstate = pState::STOPPED;
+    std::cout << "Playback stopped" << std::endl;
+}
+
+void MPlayer::pause() {
+    if (curtrack == -1) return;
+    if (pstate == pState::STOPPED) return;
+    if (pstate == pState::PAUSED) {
+        pstate = pState::PLAYBACK;
+        std::cout << "Playback resumed" << std::endl;
+        return;
+    }
+    pstate = pState::PAUSED;
+    std::cout << "Playback paused" << std::endl;
+}
+
+void MPlayer::play() {
+    if (pstate != pState::PLAYBACK and curtrack != -1) pstate = pState::PLAYBACK;
+}
+
+void MPlayer::play(int trackidx) {
+    curtrack = trackidx;
+    if (pstate != pState::PLAYBACK) pstate = pState::PLAYBACK;
+}
+
+void MPlayer::next() {
+    if (curtrack != trackList.size()-1) {
+        curtrack++;
+        if (pstate != pState::PLAYBACK) pstate = pState::PLAYBACK;
+        std::cout << "Now played: " << trackList[curtrack]->getName() << std::endl;
+    }
+}
+
+void MPlayer::addtrack(std::string tname, std::string dateCr, unsigned int tlength) {
+
 }
