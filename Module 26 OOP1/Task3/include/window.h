d26667d586e7664c081d5d303e5fd98770232dd2
Artem Ulyanov <aiden_pride@mail.ru>
2023-02-22 00:26:21 +0300

    OOP1 HT3 Added

---
+++ Module 26 OOP1/Task3/include/window.h
@@ -0,0 +1,8 @@
+//
+// Created by Artem on 22.02.2023.
+//
+
+#ifndef MOD26_OOP_HT3_WINDOW_H
+#define MOD26_OOP_HT3_WINDOW_H
+
+#endif //MOD26_OOP_HT3_WINDOW_H
---
+++ Module 26 OOP1/Task3/src/window.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Artem on 22.02.2023.
+//
+
+#include "window.h"
