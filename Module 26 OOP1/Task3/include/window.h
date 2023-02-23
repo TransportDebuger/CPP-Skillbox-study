79285ca93072d5d87df05daee3d34be4ed25f8bc
Artem Ulyanov <aiden_pride@mail.ru>
2023-02-23 23:22:12 +0300

    Mod26 ht3

--- Module 26 OOP1/Task3/include/window.h
+++ Module 26 OOP1/Task3/include/window.h
@@ -5,4 +5,15 @@
 #ifndef MOD26_OOP_HT3_WINDOW_H
 #define MOD26_OOP_HT3_WINDOW_H
 
+
+class window {
+    int height;  //Specifies the vertical size of the window in pixels
+    int width;       //Specifies the horizontal size of the window in pixels
+    int left;        //Specifies the Y coordinate of the upper-left corner
+    int top;         //Specifies the X coordinate of the upper-left corner
+
+public:
+    window(int hsize = 50, int wsize = 80, int lpoint = 10, int tpoint = 10);
+};
+
 #endif //MOD26_OOP_HT3_WINDOW_H
--- Module 26 OOP1/Task3/src/window.cpp
+++ Module 26 OOP1/Task3/src/window.cpp
@@ -3,3 +3,10 @@
 //
 
 #include "window.h"
+
+window::window(int hsize= 50, int wsize = 80, int lpoint = 10, int tpoint = 10) {
+    this->height = hsize;
+    this->width = wsize;
+    this->left = lpoint;
+    this->top = tpoint;
+}
